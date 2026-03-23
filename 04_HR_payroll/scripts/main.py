c16bd46c7aab4861fb604c6ba6b19cb0725c2a73
Jassed <jassedgmartinez@gmail.com>
2026-03-23 10:44:03 +0100

    feat: data transformation function was added

---
+++ 04_HR_payroll/scripts/main.py
@@ -0,0 +1,39 @@
+
+def extract_data(file_path):
+
+    raw_data = []
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            pyroll_info = line.strip().split(';')
+            raw_data.append(pyroll_info)
+        return raw_data
+    
+file_path = "04_HR_Payroll/data/empleados_raw.txt"
+
+extracted_data = extract_data(file_path)
+
+def trasnformation_data(extracted_data):
+    raw_data = []
+    
+    for row in extracted_data:
+        id_employee = row[0]
+        employee = row[1].title().strip()
+        department = row[2]
+        gross_salary = row[3]
+
+        try:
+            gross_salary = float(gross_salary)
+            gross_salary = round(gross_salary, 2)
+            net_salary = (gross_salary * 0.81) / 12
+            net_salary = round(net_salary, 2)
+            clean_data = [id_employee, employee, department, gross_salary, net_salary]
+            raw_data.append(clean_data)
+        except ValueError:
+            gross_salary = 0.0
+            employee = f"REVISAR {employee}"
+            raw_data.append(employee)
+
+    return raw_data
+
+print(trasnformation_data(extracted_data))
