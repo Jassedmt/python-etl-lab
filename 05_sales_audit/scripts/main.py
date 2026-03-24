2655d2a0663f28287757aa1feca8132bf7f29e1f
Jassed <jassedgmartinez@gmail.com>
2026-03-24 14:11:38 +0100

    feat: data transformation function added

--- 05_sales_audit/scripts/main.py
+++ 05_sales_audit/scripts/main.py
@@ -11,4 +11,30 @@ def extract_data(file_path):
 file_path = '05_sales_audit/data/ventas_raw.csv'
 
 extracted_data = extract_data(file_path)
-print(extracted_data)
+
+def data_transformation(extracted_data):
+
+    clean_data = []
+
+    for row in extracted_data:
+        id_prod = row[0]
+        name_prod = row[1]
+        total_income = row[2]
+        units_prod = row[3]
+
+        try:
+            total_income = float(total_income)
+            units_prod = int(units_prod)
+            unit_price = total_income/units_prod
+        except(ValueError, ZeroDivisionError):
+            name_prod = f"REVISAR {name_prod}"
+            total_income = 0.0
+            unit_price = 0.0
+
+        data_cleaned = [id_prod, name_prod, total_income, units_prod, unit_price]
+        clean_data.append(data_cleaned)
+
+    return clean_data
+
+transformed_data = data_transformation(extracted_data)
+print(transformed_data)
