3c34716e1de5496d5eda3f06ec301dc74d2c2cd2
Jassed <jassedgmartinez@gmail.com>
2026-03-27 22:39:35 +0100

    feat: extraction function implemented

---
+++ 07_multichannel_sales_integration/scripts/main.py
@@ -0,0 +1,41 @@
+import os
+import json
+import csv
+
+def extract_data(file_path, file_type):
+    raw_data = []
+
+    with open(file_path, "r", encoding='utf-8') as file:
+
+        if file_type == "csv":
+            reader = csv.DictReader(file)
+            for row in reader:
+                row[data_path] = os.path.basename(file_path)
+                raw_data.append(row)
+
+        elif file_type == "json":
+            data = json.load(file)
+            for row in data:
+                row[data_path]= os.path.basename(file_path)
+                raw_data.append(row)
+
+        return raw_data
+    
+
+data_path = "07_multichannel_sales_integration/data/"
+all_extracted_data = []
+
+for file_name in os.listdir(data_path):
+    if file_name.endswith(('.csv', '.json')):
+        full_path = os.path.join(data_path, file_name)
+
+        if file_name.endswith(".csv"):
+            file_type = "csv"
+        else:
+            file_type = "json"
+
+        data_from_file = extract_data(full_path, file_type)
+
+        all_extracted_data.extend(data_from_file)
+
+print(f"Filas totales: {len(all_extracted_data)}")
